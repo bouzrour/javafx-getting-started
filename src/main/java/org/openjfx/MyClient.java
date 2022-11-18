5054c2b58141f1f94fd4acf4e70925e38b6c08fd
Reda <redabouzrour@gmail.com>
2022-11-18 11:58:30 +0100

    Com IP

--- src/main/java/org/openjfx/MyClient.java
+++ src/main/java/org/openjfx/MyClient.java
@@ -5,7 +5,7 @@ import java.net.*;
 public class MyClient {
     public static void main(String [] args ) {
         try{
-            Socket s = new Socket("localhost",6666);
+            Socket s = new Socket("10.10.24.64",6666);
             DataOutputStream dout = new DataOutputStream(s.getOutputStream ());
             dout.writeUTF(" Hello Server ");
             dout.flush();
--- src/main/java/org/openjfx/PrimaryController.java
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -1,6 +1,9 @@
 package org.openjfx;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.Socket;
+
 import javafx.fxml.FXML;
 
 public class PrimaryController {
@@ -9,11 +12,30 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
     @FXML
-    MyChart myChart = new MyChart ();
+    MyChart myChart = new MyChart();
+
+
+    @FXML
     private void initialize() {
         ChartUpdater server = new ChartUpdater(myChart);
         server.setDaemon(true);
         server.start();
+    }
+
+
+    @FXML
+    public static void addClient() throws IOException {
+        try {
+            Socket s = new Socket("localhost", 6666);
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            dout.writeUTF(" 0.3 ");
+            dout.flush();
+            dout.close();
+            s.close();
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
 }
